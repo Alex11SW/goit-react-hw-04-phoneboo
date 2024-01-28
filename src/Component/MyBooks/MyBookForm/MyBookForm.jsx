78e20c5367d2a9d50c67ed1d970cc11b508ae2e5
alex_shklyar <136641877+Alex11SW@users.noreply.github.com>
2024-01-28 16:22:17 +0200

    new

--- src/Component/MyBooks/MyBookForm/MyBookForm.jsx
+++ src/Component/MyBooks/MyBookForm/MyBookForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import { nanoid } from "nanoid";
 import styles from "./my-book-form.module.css";
 
@@ -7,7 +7,81 @@ const INITIAL_STATE = {
   name: "",
   number: "",
 };
+const MyBookForm = ({ onSubmit }) => {
+  const [state, setState] = useState({ ...INITIAL_STATE });
+  const bookTitleId = nanoid();
+  const bookAuthorId = nanoid();
 
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    if (name === "number") {
+      let formattedNumber = value.replace(/\D/g, "");
+      if (formattedNumber.length > 2) {
+        formattedNumber = formattedNumber.replace(/(\d{3})(\d{2})/, "$1-$2");
+      }
+      if (formattedNumber.length > 5) {
+        formattedNumber = formattedNumber.replace(
+          /(\d{3})-(\d{2})(\d{2})/,
+          "$1-$2-$3"
+        );
+      }
+      setState({
+        ...state,
+        [name]: formattedNumber,
+      });
+    } else {
+      setState({
+        ...state,
+        [name]: value,
+      });
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ ...state });
+    reset();
+  };
+  const reset = () => {
+    setState({ ...INITIAL_STATE });
+  };
+
+  // const { bookTitleId, bookAuthorId, handleSubmit, handleChange } = state;
+  const { name, number } = state;
+  return (
+    <div className={styles.books}>
+      <h1>Phonebook</h1>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.formGroup}>
+          <label htmlFor={bookTitleId}>Name</label>
+          <input
+            value={name}
+            onChange={handleChange}
+            id={bookTitleId}
+            type="text"
+            name="name"
+            placeholder="Name"
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor={bookAuthorId}>Number</label>
+          <input
+            value={number}
+            onChange={handleChange}
+            id={bookAuthorId}
+            type="tel"
+            pattern="[0-9-]*"
+            name="number"
+            placeholder="Number"
+            required
+          />
+        </div>
+        <button type="submit">Add contact</button>
+      </form>
+    </div>
+  );
+};
+/*
 class MyBookForm extends Component {
   bookTitleId = nanoid();
   bookAuthorId = nanoid();
@@ -15,10 +89,6 @@ class MyBookForm extends Component {
   state = { ...INITIAL_STATE };
 
   handleChange = ({ target }) => {
-    // const { name, value } = target;
-    // this.setState({
-    //     [name]: value
-    // }
     const { name, value } = target;
     if (name === "number") {
       let formattedNumber = value.replace(/\D/g, "");
@@ -86,5 +156,5 @@ class MyBookForm extends Component {
     );
   }
 }
-
+*/
 export default MyBookForm;
--- src/Component/MyBooks/MyBookList/MyBookList.jsx
+++ src/Component/MyBooks/MyBookList/MyBookList.jsx
@@ -1,6 +1,11 @@
 import styles from "./my-book-list.module.css";
 
 const MyBookList = ({ items, deleteBook }) => {
+  if (!Array.isArray(items)) {
+    // Если items не является массивом, выводим сообщение об ошибке и возвращаем null
+    console.error("Items is not an array:", items);
+    return null;
+  }
   const elements = items.map(({ id, name, number }) => (
     <li key={id}>
       {name}: {number}
